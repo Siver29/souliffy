9cdf161c40993802b6e5924fe5e7a582775cd5c5
Salma Abo Shkair <salma1aboshkair1@gmail.com>
2024-08-06 17:40:14 +0300

    navbar

--- src/Components/UserComponents/Navbar/Navbar.js
+++ src/Components/UserComponents/Navbar/Navbar.js
@@ -7,18 +7,18 @@ const Navbar = () => {
         <div className='container'>
             <header className="header_section long_section px-0 ">
                 <nav className="navbar navbar-expand-lg custom_nav-container ">
-
                     <a className=" navbar-brand ml-3" href="index.html">
                         <img className='img-fluid' width='110' src='./Assets/img/souliffy-logo-primary-text-color.png'/>
                     </a>
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className=""> </span>
                     </button>
+
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <div className="d-flex flex-column flex-lg-row align-items-center">
                             <ul className="navbar-nav ">
                                 <li className="nav-item active">
-                                    <a className="nav-link active" href="#slider">Home</a>
+                                    <a className="nav-link active" href="#slider">الرئيسية</a>
                                 </li>
                                 <li className="nav-item">
                                     <a className="nav-link" href="#about"> About us</a>
