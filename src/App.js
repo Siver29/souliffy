33663ce16e896cfbe671683a980dd80d208e539b
Siver29 <mohammad111khayat@gmail.com>
2024-08-07 14:03:27 +0300

    calender

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Reservation from './Pages/Reservation/Reservation';
 import Navbar from './Components/UserComponents/Navbar/Navbar'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      {/* <Navbar/> */}
+      <Reservation/>
     </div>
   );
 }
---
+++ src/Pages/Reservation/Reservation.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import CalenderWrapper from '../../Components/Calendar/CalenderWrapper'
+
+function Reservation() {
+  return (
+    <section className='reservation-page'>
+      <CalenderWrapper/>
+    </section>
+  )
+}
+
+export default Reservation
