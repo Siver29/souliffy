4cf37bbc3340e98050dee0f9b53ffaaadc68f2ac
Siver29 <mohammad111khayat@gmail.com>
2024-08-07 17:39:42 +0300

    calender v.2

---
+++ src/Components/Times/Times.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Times.css'
+
+function Times({time}) {
+  return (
+    <div className="col-6 col-md-3 mb-2">
+      <button className='btn timeCard'>{time}</button>
+    </div>
+  )
+}
+
+export default Times
--- src/Pages/Reservation/Reservation.js
+++ src/Pages/Reservation/Reservation.js
@@ -1,10 +1,24 @@
 import React from 'react'
 import CalenderWrapper from '../../Components/Calendar/CalenderWrapper'
+import Times from '../../Components/Times/Times'
+import './Reservation.css'
 
 function Reservation() {
+  const times = ['10:00AM', '11:00AM', '12:00PM', '1:00PM', '2:00PM', '3:00PM', '4:00PM'
+    , '5:00PM', '6:00PM', '7:00PM', '8:00PM', '9:00PM', '10:00PM', '11:00PM']
   return (
     <section className='reservation-page'>
-      <CalenderWrapper/><p>cccc</p>
+      <div className='container reservation-bg'>
+        <CalenderWrapper />
+        <div className=' w-50 mt-5'>
+          <p>إختر الوقت</p>
+          <div className='row'>
+            {times.map((time, index) => (
+              <Times key={index} time={time} />
+            ))}
+          </div>
+        </div>
+      </div>
     </section>
   )
 }
