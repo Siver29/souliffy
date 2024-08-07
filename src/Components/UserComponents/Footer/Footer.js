39fffe80a7fab1fb5595fbb62181384c9596ad81
Raneem-Shaban <raneemshaban11@gmail.com>
2024-08-07 14:18:32 +0300

    Testmonials

--- src/Components/UserComponents/Footer/Footer.js
+++ src/Components/UserComponents/Footer/Footer.js
@@ -4,49 +4,74 @@ import './Footer.css';
 
 const Footer = () => {
   return (
-    <div className="container-fluid footer mt-5 p-5">
-      <div className="container">
+    <div className="container-fluid footer p-4">
+      <div className="container h-75">
         {/* ........... */}
-        <div className="col-md-6 border-bottom border-top bg-light text-dark pt-4">
-          <div className="col-md-4 mb-3">
-            <p>
-              <img className='' src='assets/svg/round-place.svg' alt='Location' /> دمشق،سورية
-            </p>
+        <div className='row h-100'>
+          {/* contact us */}
+          <div className="col-md-6 col-12 text-end">
+            <p className=''>تواصل معنا</p>
+            <form className='w-75 contact-form'>
+              <div className="form-group pb-2">
+                <input type="text" className="form-control rounded-2" id="الاسم" placeholder="Name" />
+              </div>
+              <div className="form-group pb-2">
+                <input type="email" className="form-control rounded-2" id="الايميل" placeholder="Email" />
+              </div>
+              <div className="form-group pb-2">
+                <textarea className="form-control rounded-2 textarea-height " id="الرسالة" rows="5" placeholder="Message"></textarea>
+              </div>
+              <button type="submit" className="btn-submit btn rounded-2 w-100">إرسال</button>
+            </form>
           </div>
-          <div className="col-md-4 mb-3">
-            <p>
-              <img src='assets/svg/round-phone.svg' alt='Phone' /> (123) 456-7890
-            </p>
+          {/* contact info */}
+          <div className="contact-info col-md-6 col-sm-12 text-dark">
+            <div className="col-md-6">
+              <p className='text-end place'>
+                <img className='' src='assets/svg/round-place.svg' alt='Location' /> سورية، دمشق
+              </p>
+            </div>
+            <div className='container'>
+              <div className='row'>
+                <div className="col-md-6 col-12">
+                  <p className='text-end'>
+                    <img src='assets/svg/round-phone.svg' alt='Phone' /> (123) 456-7890
+                  </p>
+                </div>
+                <div className="col-md-6 col-12">
+                  <p className='text-end'>
+                    <img src='assets/svg/round-local-printshop.svg' alt='Fax' /> (123) 456-7890
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="row text-center">
+              <div className="container">
+                <div className='row'>
+                  <div className='col-4'>
+                    <p>مواقع التواصل</p>
+                  </div>
+                  <div className='col-2'>
+                    <a href="https://www.facebook.com" className="text-dark ">
+                      <img src='assets/svg/facebook.svg' alt='Facebook' />
+                    </a>
+                  </div>
+                  <div className='col-2'>
+                    <a href="https://www.instagram.com" className="text-dark">
+                      <img src='assets/svg/instagram.svg' alt='Instagram' />
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
-          <div className="col-md-4 mb-3">
-            <p>
-              <img src='assets/svg/round-local-printshop.svg' alt='Fax' /> (123) 456-7890
-            </p>
-          </div>
+
         </div>
         {/* ............... */}
-        <div className="row text-center mt-4">
-          <div className="col">
-            <h5>Social Media</h5>
-            <a href="https://www.facebook.com" className="text-dark mx-2">
-              <img src='assets/svg/facebook.svg' alt='Facebook' />
-            </a>
-            <a href="https://www.linkedin.com" className="text-dark mx-2">
-              <img src='assets/svg/linkedin.svg' alt='LinkedIn' />
-            </a>
-            <a href="https://www.youtube.com" className="text-dark mx-2">
-              <img src='assets/svg/youtube.svg' alt='YouTube' />
-            </a>
-            <a href="https://www.instagram.com" className="text-dark mx-2">
-              <img src='assets/svg/instagram.svg' alt='Instagram' />
-            </a>
-          </div>
-        </div>
-        <div className="row mt-3">
+        <div className="row mt-5  latest-footer">
           <div className="col-12 col-md-6">
             <div className="d-flex justify-content-around align-items-center">
               <a href="#about" className="text-dark footer-links">ABOUT US</a>
-              <a href="#contact" className="text-dark footer-links">CONTACT US</a>
               <a href="#platform" className="text-dark footer-links">PLATFORM</a>
             </div>
           </div>
