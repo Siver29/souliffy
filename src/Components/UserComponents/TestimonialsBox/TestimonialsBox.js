9fccc7ffd200d514767bea2d548d74333cad76d4
Raneem-Shaban <raneemshaban11@gmail.com>
2024-08-08 12:10:37 +0300

    testimonials slider

--- src/Components/UserComponents/TestimonialsBox/TestimonialsBox.js
+++ src/Components/UserComponents/TestimonialsBox/TestimonialsBox.js
@@ -2,32 +2,57 @@
 import React from 'react';
 import './TestimonialsBox.css';
 
-function TestmonialsBox({ quote }) {
+function TestmonialsBox({ testmonialText, bgColor, triangleColor, quote }) {
+    const triangleStyleBefore = {
+        content: '',
+        width: 0,
+        height: 0,
+        borderStyle: 'solid',
+        position: 'absolute',
+        bottom: '-20px',
+        top: '250px',
+        left: '0px',
+        borderWidth: '110px 175px 0 0',
+        borderColor: `${triangleColor} transparent transparent transparent`
+    };
+
+    const triangleStyleAfter = {
+        content: '',
+        width: 0,
+        height: 0,
+        borderStyle: 'solid',
+        position: 'absolute',
+        bottom: '-20px',
+        right: '-0px',
+        top: '250px',
+        borderWidth: '110px 0 0 175px',
+        borderColor: `${triangleColor} transparent transparent transparent`
+    };
+
     return (
-        <div className="container mt-5 TestmonialsBox">
-            <div className="row h-100 text-center quote-box">
-                <div className='container-fluid mt-5'>
-                    <div className='row'>
-                        <div className='col-1'></div>   
-                        <div className='col-3'>
-                            <img src='assets/svg/quote-line.svg' className='img-fluid'></img>
-                        </div>
-                        <div className='col-8'></div>
+        <div className="quote-shape" style={{ backgroundColor: bgColor }}>
+            <div style={triangleStyleBefore}></div>
+            <div style={triangleStyleAfter}></div>
+            <div className='container-fluid mt-3'>
+                <div className='row'>
+                    <div className='col-1'></div>
+                    <div className='col-3'>
+                        <img src={quote} className='img-fluid' alt='quote' />
                     </div>
+                    <div className='col-8'></div>
                 </div>
-                <p className="mb-0 quote">
-                    {quote} انطلقنا من إيماننا العميق بضرورة توفير الدعم النفسي المتخصص للمجتمع يعاني 
-                </p>
-                <div className='container-fluid'>
-                    <div className='row'>
-                        <div className='col-8'></div>   
-                        <div className='col-3'>
-                            <img src='assets/svg/quote-line.svg' className='img-fluid quote-img'></img>
-                        </div>
-                        <div className='col-1'></div>
+            </div>
+            <p className="quote-text">
+                {testmonialText}
+            </p>
+            <div className='container-fluid'>
+                <div className='row'>
+                    <div className='col-8'></div>
+                    <div className='col-3'>
+                        <img src={quote} className='img-fluid quote-img' alt='quote' />
                     </div>
+                    <div className='col-1'></div>
                 </div>
-                <div className="triangle"></div>
             </div>
         </div>
     );
--- src/Pages/UserPages/HomePage/HomePage.js
+++ src/Pages/UserPages/HomePage/HomePage.js
@@ -3,12 +3,20 @@ import Footer from '../../../Components/UserComponents/Footer/Footer'
 import Navbar from '../../../Components/UserComponents/Navbar/Navbar'
 import TestmonialsBox from '../../../Components/UserComponents/TestimonialsBox/TestimonialsBox'
 import TestimonialsContainer from '../../../Components/UserComponents/TestimonialsContainer/TestimonialsContainer'
+import TestimonialsSlider from '../../../Components/UserComponents/TestimonialsSlider/TestimonialsSlider'
 
 const HomePage = () => {
+  const testmonials = [
+    "Souliffy أعادت لي شغفي بالحياة وأبعدت عني الاكتئاب.",
+    "التوتر والقلق اختفوا بعد انضمامي لـ Souliffy. الدعم هنا غير حياتي",
+    "تعلمت كيفية التعامل مع التوتر وزيادة قوتي النفسية بفضل Souliffy"
+];
   return (
     <div>
       <Navbar/>
-      <TestimonialsContainer/>
+      {/* <TestimonialsContainer/> */}
+      {/* <TestmonialsBox testmonialText={testmonials[1]} bgColor="var(--primary-blue-color)" triangleColor="var(--primary-blue-color)"/> */}
+      <TestimonialsSlider/>
       <Footer/>
     </div>
   )
