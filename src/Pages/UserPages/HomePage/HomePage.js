fadbb88264eceddb7ac55220aa3c6a09f7a65927
Salma Abo Shkair <salma1aboshkair1@gmail.com>
2024-08-08 16:33:48 +0300

    Hero

--- src/Pages/UserPages/HomePage/HomePage.js
+++ src/Pages/UserPages/HomePage/HomePage.js
@@ -1,21 +1,13 @@
 import React from 'react'
 import Hero from '../../../Components/UserComponents/HeroSection/Hero'
 import About from '../../../Components/UserComponents/AboutSection/About';
-import Footer from '../../../Components/UserComponents/Footer/Footer'
-import Navbar from '../../../Components/UserComponents/Navbar/Navbar'
-import TestmonialsBox from '../../../Components/UserComponents/TestimonialsBox/TestimonialsBox'
-import TestimonialsContainer from '../../../Components/UserComponents/TestimonialsContainer/TestimonialsContainer'
-import TestimonialsSlider from '../../../Components/UserComponents/TestimonialsSlider/TestimonialsSlider'
-import Article from '../../../Components/UserComponents/Article/Article'
 import ArticlePage from '../../../Components/UserComponents/ArticlePage/ArticlePage'
-import Doctor from '../../../Components/UserComponents/Doctor/Doctor'
 import DoctorPage from '../../../Components/UserComponents/DoctorPage/DoctorPage'
 
 const HomePage = () => {
 
   return (
     <div>
-      {/* <Navbar/> */}
       <Hero/>
       {/* <TestimonialsContainer/> */}
       <About/>
@@ -23,7 +15,6 @@ const HomePage = () => {
       <DoctorPage/>
       <ArticlePage/>
       {/* <TestimonialsSlider/> */}
-      <Footer/>
     </div>
   )
 }
