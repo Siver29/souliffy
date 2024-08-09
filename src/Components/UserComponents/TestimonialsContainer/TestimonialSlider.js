ee1b0d3fe0f85de6572b48afef299d50b8a2e0aa
Siver29 <mohammad111khayat@gmail.com>
2024-08-10 01:39:42 +0300

    testimonials last

--- src/Components/UserComponents/TestimonialsContainer/TestimonialSlider.js
+++ src/Components/UserComponents/TestimonialsContainer/TestimonialSlider.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React , { useRef, useEffect ,useState }from 'react';
 import './TestimonialSlider.css';
 import TestimonialsBox from '../TestimonialsBox/TestimonialsBox';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { EffectCoverflow, Pagination, Navigation } from 'swiper/modules';
+
 
 import 'swiper/css';
 import 'swiper/css/effect-coverflow';
@@ -10,6 +11,32 @@ import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 
 function TestimonialSlider() {
+
+  // const [currentSlide, setCurrentSlide] = useState(0);
+
+  // const handleSlideChange = (swiper) => {
+  //   setCurrentSlide(swiper.activeIndex);
+  // };
+  // useEffect(() => {
+  //   const leftShadowElement = document.querySelector('.swiper-slide-shadow-left');
+  //   const rightShadowElement = document.querySelector('.swiper-slide-shadow-right');
+  //   const coverFlowShadowElement = document.querySelector('.swiper-slide-shadow-coverflow');
+
+  //   if (leftShadowElement) {
+  //     console.log(1)
+  //     leftShadowElement.style.opacity = 0;
+  //   }
+
+  //   if (rightShadowElement) {
+  //     console.log(2)
+  //     rightShadowElement.style.opacity = 0;
+  //   }
+
+  //   if (coverFlowShadowElement) {
+  //     console.log(2)
+  //     coverFlowShadowElement.style.opacity = 0;
+  //   }
+  // }, [currentSlide]);
   const testimonials = [
     "Souliffy أعادت لي شغفي بالحياة وأبعدت عني الاكتئاب.",
     "التوتر والقلق اختفوا بعد انضمامي لـ Souliffy. الدعم هنا غير حياتي",
@@ -20,17 +47,19 @@ function TestimonialSlider() {
 <div className="container">
       <h1 className="heading">آراء المستخدمين</h1>
       <Swiper
+        // onSlideChange={handleSlideChange}
         effect={'coverflow'}
         grabCursor={true}
         centeredSlides={true}
         loop={true}
-        // slidesPerView={3}
+        slidesPerView={3}
         spaceBetween={0}
         coverflowEffect={{
           rotate: 0,
           stretch: 0,
           depth: 100,
           modifier: 2.5,
+          slideShadows : false
         }}
         pagination={{ el: '.swiper-pagination', clickable: true }}
         navigation={{
@@ -75,12 +104,12 @@ function TestimonialSlider() {
         </SwiperSlide>
 
         <div className="slider-controler">
-          <div className="swiper-button-prev slider-arrow">
+          {/* <div className="swiper-button-prev slider-arrow">
             <ion-icon name="arrow-back-outline"></ion-icon>
           </div>
           <div className="swiper-button-next slider-arrow">
             <ion-icon name="arrow-forward-outline"></ion-icon>
-          </div>
+          </div> */}
           <div className="swiper-pagination"></div>
         </div>
       </Swiper>
