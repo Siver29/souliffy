a567735ce907baf3b0d1c7878332d48c171e283b
Siver29 <mohammad111khayat@gmail.com>
2024-08-09 17:07:13 +0300

    slider v.1

--- src/Pages/UserPages/HomePage/HomePage.js
+++ src/Pages/UserPages/HomePage/HomePage.js
@@ -3,19 +3,20 @@ import Hero from '../../../Components/UserComponents/HeroSection/Hero'
 import About from '../../../Components/UserComponents/AboutSection/About';
 import ArticlePage from '../../../Components/UserComponents/ArticlePage/ArticlePage'
 import DoctorPage from '../../../Components/UserComponents/DoctorPage/DoctorPage'
+import TestimonialsSlider from '../../../Components/UserComponents/TestimonialsSlider/TestimonialsSlider';
+import TestimonialSlider from '../../../Components/UserComponents/TestimonialsContainer/TestimonialSlider';
 
 const HomePage = () => {
 
   return (
     <div>
-      <p>salma</p>
       <Hero/>
       {/* <TestimonialsContainer/> */}
       <About/>
       {/* <Article/> */}
       <DoctorPage/>
       <ArticlePage/>
-      {/* <TestimonialsSlider/> */}
+      {/* <TestimonialSlider/> */}
     </div>
   )
 }
